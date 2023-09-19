b6f5f310ca7aa722efd1208f82610ad613f3b157
Pedrohss-hub <pedrohss466@gmail.com>
2023-09-19 11:39:36 -0300

    Php + Bd

---
+++ Integração ao BD/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Pesquisa de Modelos de Carros</h1>
+
+    <form action="" method="GET">
+        <label for="pesquisa">Pesquisar Modelor</label>
+        <input type="text" name="pesquisa" id="pesquisa" placeholder="Digite o modelo">
+        <input type="submit" value="pesquisar">
+    </form>
+
+    <?php 
+        $serveName = "localhost";
+        $userName = "root";
+        $password = "";
+        $dbName = "carros";
+
+        $con = new mysqli($serveName, $userName, $password, $dbName);
+
+        if($con -> connect_error) {
+            die("Conexão falhou: ".$con->connect_error);
+        }
+
+        if(isset($_GET['pesquisa']) && !empty($_GET['pesquisa'])) {
+            $pesquisa = $_GET['pesquisa'];
+            $sql = "SELECT * FROM carros WHERE modelo LIKE '%$pesquisa%'";
+            $result = $con -> query($sql);
+
+            if($result -> num_rows > 0) {
+                echo "<h2>Resultado da Pesquisa: </h2>";
+                echo "<ul>";
+                while($row = $result->fetch_assoc()) {
+                    echo "<li>{$row['marca']} - {$row['modelo']} ({$row['ano']})</li>";
+                }
+                echo "</ul>";
+            } else {
+                echo "<p>...</p>";
+            }
+        }
+
+        $con -> close();
+    ?>
+</body>
+</html>
